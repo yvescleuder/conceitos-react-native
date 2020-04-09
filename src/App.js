f978fc2f60abf8e8f0a0fa3e2ac07af1553b62cf
Yves Cleuder <yves.cl@live.com>
2020-04-08 23:15:44 -0300

    Teste finalizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SafeAreaView,
@@ -9,47 +9,79 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import api from "./services/api";
 
 export default function App() {
+
+  const [ repositories, setRepositories] = useState([]);
+
+  /**
+   * Ao abrir a aplicacao, ira buscar todos os repositories da API.
+   */
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    await api.post(`repositories/${id}/like`);
+
+    /**
+     * Faco um loop buscando o ID que estou querendo editar, se achar vou editar o likes,
+     * se nao so vou retorna ele mesmo
+     */
+    const newRepositories = repositories.map(
+      (repository) => repository.id === id ? { ...repository, likes: repository.likes += 1} : repository
+    );
+    
+    setRepositories(newRepositories);
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+      
+        <FlatList 
+            data={repositories}
+            keyExtractor={repository => repository.id}
+            renderItem={({ item: repository }) => {
+              return (
+                <View style={styles.repositoryContainer}>
+                  <Text style={styles.repository}>{repository.title}</Text>
+                  
+                  <View style={styles.techsContainer}>
+                    {repository.techs.map(tech => (
+                        <Text key={tech} style={styles.tech}>
+                          {tech}
+                        </Text>
+                    ))}
+                  </View>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+                  <View style={styles.likesContainer}>
+                    <Text
+                      style={styles.likeText}
+                      // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                      testID={`repository-likes-${repository.id}`}
+                    >
+                      {repository.likes} curtidas
+                    </Text>
+                  </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+                  <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.6}
+                    onPress={() => handleLikeRepository(repository.id)}
+                    // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                    testID={`like-button-${repository.id}`}
+                  >
+                    <Text style={styles.buttonText}>Curtir</Text>
+                  </TouchableOpacity>
+                </View>
+              )
+            }}
+        />
       </SafeAreaView>
     </>
   );
